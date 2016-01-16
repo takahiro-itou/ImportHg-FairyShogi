59617c289c28975d93363913837e00d1d8672fae
Takahiro Itou <Takahiro Itou@localhost>
2016-01-16 14:23:46 +0900

    ????????

---
+++ Bin/FairyShogi.cpp
@@ -0,0 +1,29 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      アプリケーション本体。
+**
+**      @file       Bin/FairyShogi.cpp
+**/
+
+#include    "FairyShogi/Common/FairyShogiTypes.h"
+
+#include    <iostream>
+
+using   namespace   FAIRYSHOGI_NAMESPACE;
+
+int  main(int argc, char * argv[])
+{
+    std::cout   <<  "Fairy Shogi Version 0.0"
+                <<  std::endl;
+    return ( 0 );
+}
