456d9ae275d2323b9d591a5b056ba7389423feab
Takahiro Itou <Takahiro Itou@localhost>
2016-01-16 14:21:01 +0900

    ??????????

---
+++ Include/FairyShogi/Common/FairyShogiSettings.h
@@ -0,0 +1,28 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      プロジェクトの設定。
+**
+**      @file       Common/FairyShogiSettings.h
+**/
+
+#if !defined( FAIRYSHOGI_COMMON_INCLUDED_FAIRY_SHOGI_SETTINGS_H )
+#    define   FAIRYSHOGI_COMMON_INCLUDED_FAIRY_SHOGI_SETTINGS_H
+
+//  スクリプトによる設定値が書き込まれたヘッダを読み込む。  //
+#include    "FairyShogi/.Config/ConfiguredFairyShogi.h"
+
+FAIRYSHOGI_NAMESPACE_BEGIN
+
+FAIRYSHOGI_NAMESPACE_END
+
+#endif
---
+++ Include/FairyShogi/Common/FairyShogiTypes.h
@@ -0,0 +1,34 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Type Definitions.
+**
+**      @file       Common/FairyShogiTypes.h
+**/
+
+#if !defined( FAIRYSHOGI_COMMON_INCLUDED_FAIRY_SHOGI_TYPES_H )
+#    define   FAIRYSHOGI_COMMON_INCLUDED_FAIRY_SHOGI_TYPES_H
+
+#include    "FairyShogiSettings.h"
+
+#include    <stdint.h>
+
+FAIRYSHOGI_NAMESPACE_BEGIN
+
+//========================================================================
+//
+//    Type Definitions.
+//
+
+FAIRYSHOGI_NAMESPACE_END
+
+#endif
