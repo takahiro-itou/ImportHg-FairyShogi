6cbe958e32c165329057017bffdb38018c634a0c
Takahiro Itou <Takahiro Itou@localhost>
2016-01-17 19:54:26 +0900

    ??????????

--- Include/FairyShogi/Common/FairyShogiSettings.h
+++ Include/FairyShogi/Common/FairyShogiSettings.h
@@ -23,6 +23,47 @@
 
 FAIRYSHOGI_NAMESPACE_BEGIN
 
+//========================================================================
+//
+//    FSSYSLIMIT  enum.
+//
+/**
+**    ゲームシステムの制限事項をまとめた定数用列挙型。
+**/
+
+enum  FSSYSLIMIT
+{
+    /**
+    **    プログラムが扱える最大の盤面サイズ（横方向）。
+    **/
+    MAX_FIELD_COLS  = 16,
+
+    /**
+    **    プログラムが扱える最大の盤面サイズ（縦方向）。
+    **/
+    MAX_FIELD_ROWS  = 16,
+
+    /**
+    **    プログラムが扱える最大の盤面サイズ。
+    **/
+    MAX_FIELD_SIZE  = MAX_FIELD_COLS * MAX_FIELD_ROWS,
+
+    /**
+    **    プログラムが扱える最大のプレーヤー数。
+    **/
+    MAX_NUM_PLAYERS = 16,
+
+    /**
+    **    定義可能な盤上駒の種類の最大値。
+    **/
+    MAX_PIECE_TYPES = 256,
+
+    /**
+    **    定義可能な持ち駒の種類の最大値。
+    **/
+    MAX_HAND_TYPES  = 256
+};
+
 FAIRYSHOGI_NAMESPACE_END
 
 #endif
