92a5efef17ec9d638cc245cce22819ccfc3e04fd
Takahiro Itou <Takahiro Itou@localhost>
2016-02-13 19:01:20 +0900

    ????????????

--- Bin/Win32/FairyShogiWin32.cpp
+++ Bin/Win32/FairyShogiWin32.cpp
@@ -53,66 +53,12 @@ Interface::BoardScreen  g_scrBoard;
 /**   マウスのキャプチャフラグ。    **/
 HWND                    g_hCapture;
 
-/**
-**    画面に表示する駒の名称。
-**/
-
-constexpr   const   char  *
-s_tblPieceName[]    = {
-    "",
-    "Pawn ",  "Sil. ",  "Gold ",  "Bis. ",  "Rook ",  "King ",
-    "P.Pw ",  "P.Si ",  "P.Bi ",  "P.Rk ",
-    "PawnV",  "Sil.V",  "GoldV",  "Bis.V",  "RookV",  "KingV",
-    "P.PwV",  "P.SiV",  "P.BiV",  "P.RkV"
-};
-
-/**
-**    画面に表示する持ち駒の名称。
-**/
-
-constexpr   const   char  *
-s_tblHandName[]     =  {
-    nullptr,
-    "P",  "S",  "G",  "B",  "R",  "K",
-    "P",  "S",  "G",  "B",  "R",  "K"
-};
-
-//
-//    ダミー定数。
-//
-
-constexpr   int     POS_NUM_COLS        =  5;
-constexpr   int     POS_NUM_ROWS        =  5;
-
-constexpr   int     HANDS_BLACK_PAWN    = Game::BoardState::HAND_BLACK_PAWN;
-constexpr   int     HANDS_BLACK_KING    = Game::BoardState::HAND_BLACK_KING;
-constexpr   int     HANDS_WHITE_PAWN    = Game::BoardState::HAND_WHITE_PAWN;
-constexpr   int     HANDS_WHITE_KING    = Game::BoardState::HAND_WHITE_KING;
-
-
-constexpr   PieceIndex
-s_tblHandEncBlack[] = {
-    Game::BoardState::HAND_BLACK_PAWN,
-    Game::BoardState::HAND_BLACK_SILVER,
-    Game::BoardState::HAND_BLACK_GOLD,
-    Game::BoardState::HAND_BLACK_BISHOP,
-    Game::BoardState::HAND_BLACK_ROOK,
-    Game::BoardState::HAND_BLACK_KING
-};
-
-constexpr   PieceIndex
-s_tblHandEncWhite[] = {
-    Game::BoardState::HAND_WHITE_PAWN,
-    Game::BoardState::HAND_WHITE_SILVER,
-    Game::BoardState::HAND_WHITE_GOLD,
-    Game::BoardState::HAND_WHITE_BISHOP,
-    Game::BoardState::HAND_WHITE_ROOK,
-    Game::BoardState::HAND_WHITE_KING
-};
-
 //
 //    画面に関する定数。
 //
+
+constexpr   int     POS_NUM_COLS        = 5;
+constexpr   int     POS_NUM_ROWS        = 5;
 
 constexpr   int     BOARD_TOP_OFFSET    = 1;
 constexpr   int     BOARD_LEFT_OFFSET   = 0;
@@ -136,20 +82,8 @@ constexpr   int     KIFU_VIEW_LEFT      = LEFT_MARGIN
 constexpr   int     KIFU_VIEW_WIDTH     = WINDOW_WIDTH - KIFU_VIEW_LEFT;
 constexpr   int     KIFU_FONT_HEIGHT    = 24;
 
-// int     g_selX;
-// int     g_selY;
-// int     g_movX;
-// int     g_movY;
-
-// std::ofstream               ofsKifu;
-
-//Interface::GameController   gc;
-
 Interface::BitmapImage      g_imgScreen;
 Interface::BitmapImage      g_imgBoard;
-
-// Interface::BitmapImage      g_imgBack;
-// Interface::BitmapImage      g_imgPiece;
 
 }   //  End of (Unnamed) namespace.
 
@@ -178,56 +112,6 @@ onLButtonDown(
     ::SetCapture(hWnd);
     g_hCapture  = hWnd;
 
-#if 0
-    if ( (xPos < LEFT_MARGIN) || (yPos < TOP_MARGIN) ) {
-        g_selX  = -1;
-        g_selY  = -1;
-        g_movX  = -1;
-        g_movY  = -1;
-        ::InvalidateRect(hWnd, NULL, TRUE);
-        return ( 0 );
-    }
-
-    const  int  mx  = ((int)(xPos) - LEFT_MARGIN) / SQUARE_WIDTH;
-    const  int  my  = ((int)(yPos) - TOP_MARGIN) / SQUARE_HEIGHT;
-
-    if ( (VIEW_NUM_ROWS <= my) ) {
-        g_selX  = -1;
-        g_selY  = -1;
-        g_movX  = -1;
-        g_movY  = -1;
-        ::InvalidateRect(hWnd, NULL, TRUE);
-        return ( 0 );
-    }
-    if ( (my == 0) || (my == (POS_NUM_ROWS + BOARD_TOP_OFFSET)) ) {
-        if ( (HANDS_WHITE_KING - HANDS_WHITE_PAWN + 1) <= mx ) {
-            g_selX  = -1;
-            g_selY  = -1;
-            g_movX  = -1;
-            g_movY  = -1;
-            ::InvalidateRect(hWnd, NULL, TRUE);
-            return ( 0 );
-        } else {
-            g_selX  = mx;
-            g_selY  = my;
-        }
-    } else if ( (VIEW_NUM_COLS) <= mx ) {
-        g_selX  = -1;
-        g_selY  = -1;
-    } else {
-        g_selX  = mx;
-        g_selY  = my;
-    }
-
-    g_movX  = -1;
-    g_movY  = -1;
-    ::InvalidateRect(hWnd, NULL, TRUE);
-
-    if ( (g_selX >= 0) && (g_selY >= 0) ) {
-        ::SetCapture(hWnd);
-    }
-#endif
-
     return ( 0 );
 }
 
@@ -255,64 +139,6 @@ onLButtonUp(
         ::InvalidateRect(hWnd, NULL, FALSE);
     }
 
-#if 0
-    if ( (g_selX < 0) || (g_selY < 0) ) {
-        return ( 0 );
-    }
-
-    Interface::GameController::ActionViewList   actList;
-
-    const  int  mx  = ((int)(xPos) - LEFT_MARGIN) / SQUARE_WIDTH;
-    const  int  my  = ((int)(yPos) - TOP_MARGIN) / SQUARE_HEIGHT;
-
-    if ( (mx < BOARD_LEFT_OFFSET) || (my < BOARD_TOP_OFFSET)
-            || (POS_NUM_COLS + BOARD_LEFT_OFFSET <= mx)
-            || (POS_NUM_ROWS + BOARD_TOP_OFFSET  <= my) )
-    {
-        goto    label_redaw_board;
-    }
-    if ( (mx == g_selX) && (my == g_selY) ) {
-        goto    label_redaw_board;
-    }
-
-    if ( (g_selY == 0) ) {
-        //  後手の持ち駒を打つ。    //
-        gc.playPutAction(
-                mx - BOARD_LEFT_OFFSET,
-                my - BOARD_TOP_OFFSET,
-                s_tblHandEncWhite[g_selX - BOARD_LEFT_OFFSET]);
-    } else if ( g_selY == POS_NUM_ROWS + BOARD_TOP_OFFSET ) {
-        //  先手の持ち駒を打つ。    //
-        gc.playPutAction(
-                mx - BOARD_LEFT_OFFSET,
-                my - BOARD_TOP_OFFSET,
-                s_tblHandEncBlack[g_selX - BOARD_LEFT_OFFSET]);
-    } else {
-        //  盤上の駒を移動させる。  //
-        gc.playMoveAction(
-                g_selX - BOARD_LEFT_OFFSET,
-                g_selY - BOARD_TOP_OFFSET,
-                mx - BOARD_LEFT_OFFSET,
-                my - BOARD_TOP_OFFSET,
-                0);
-    }
-
-    //  最後の指し手を棋譜ファイルに書き込む。  //
-    gc.writeActionList(actList);
-    if ( ofsKifu.good() && !(actList.empty()) ) {
-        gc.writeActionView(actList.back(), ofsKifu);
-        ofsKifu << std::endl;
-        ofsKifu.flush();
-    }
-
-label_redaw_board:
-    g_selX  = -1;
-    g_selY  = -1;
-    g_movX  = -1;
-    g_movY  = -1;
-    ::InvalidateRect(hWnd, NULL, TRUE);
-#endif
-
     return ( 0 );
 }
 
@@ -338,41 +164,6 @@ onMouseMove(
         //  再描画を行う。  //
         ::InvalidateRect(hWnd, NULL, FALSE);
     }
-
-#if 0
-
-    if ( (g_selX < 0) || (g_selY < 0) ) {
-        return ( 0 );
-    }
-
-    const  int  mx  = ((int)(xPos) - LEFT_MARGIN) / SQUARE_WIDTH;
-    const  int  my  = ((int)(yPos) - TOP_MARGIN) / SQUARE_HEIGHT;
-    int     mvX, mvY;
-
-    if ( (xPos < LEFT_MARGIN) || (yPos < TOP_MARGIN) ) {
-        mvX = -1;
-        mvY = -1;
-        goto    label_redraw_board;
-    }
-
-    if ( (mx < BOARD_LEFT_OFFSET) || (my < BOARD_TOP_OFFSET)
-            || (POS_NUM_COLS + BOARD_LEFT_OFFSET <= mx)
-            || (POS_NUM_ROWS + BOARD_TOP_OFFSET  <= my) )
-    {
-        mvX = -1;
-        mvY = -1;
-    } else {
-        mvX = mx;
-        mvY = my;
-    }
-
-label_redraw_board:
-    if ( (g_movX != mvX) || (g_movY != mvY) ) {
-        g_movX  = mvX;
-        g_movY  = mvY;
-        ::InvalidateRect(hWnd, NULL, TRUE);
-    }
-#endif
 
     return ( 0 );
 }
@@ -400,78 +191,6 @@ onPaint(
 
     g_scrBoard.drawScreenLayer( &g_imgBoard );
 
-#if 0
-    Common::ViewBuffer  vb;
-    memset(&vb, 0, sizeof(vb));
-    gc.writeToViewBuffer(vb);
-
-    //  後手の持ち駒を表示する。    //
-    int     tx  = 0;
-    for ( int c = HANDS_WHITE_PAWN; c <= HANDS_WHITE_KING; ++ c, ++ tx )
-    {
-        const  THandCount   numHand = vb.nHands[c];
-        if ( numHand <= 0 ) { continue; }
-
-        dx  = (tx * SQUARE_WIDTH) + LEFT_MARGIN;
-        dy  = TOP_MARGIN;
-        sx  = (c - HANDS_WHITE_PAWN) * SQUARE_WIDTH;
-        sy  = (1) * SQUARE_HEIGHT;
-        g_imgScreen.copyRectangle(
-                dx, dy, SQUARE_WIDTH, SQUARE_HEIGHT, g_imgPiece, sx, sy);
-    }
-
-    //  盤上にある駒を表示する。    //
-    for ( int y = 0; y < POS_NUM_ROWS; ++ y ) {
-        for ( int x = 0; x < POS_NUM_COLS; ++ x ) {
-            dx  = (x * SQUARE_WIDTH) + LEFT_MARGIN;
-            dy  = ((y + BOARD_TOP_OFFSET) * SQUARE_HEIGHT) + TOP_MARGIN;
-            const  int          pi  = (y * POS_NUM_COLS) + x;
-            const  PieceIndex   dp  = vb.piBoard[pi];
-            if ( dp == 0 ) { continue; }
-
-            sx  = ((dp - 1) % 10) * SQUARE_WIDTH;
-            sy  = ((dp - 1) / 10) * SQUARE_HEIGHT;
-            g_imgScreen.copyRectangle(
-                    dx, dy, SQUARE_WIDTH, SQUARE_HEIGHT, g_imgPiece, sx, sy);
-        }
-    }
-
-    //  先手の持ち駒を表示する。    //
-    tx  = 0;
-    for ( int c = HANDS_BLACK_PAWN; c <= HANDS_BLACK_KING; ++ c, ++ tx )
-    {
-        const  THandCount   numHand = vb.nHands[c];
-        if ( numHand <= 0 ) { continue; }
-
-        dx  = (tx * SQUARE_WIDTH) + LEFT_MARGIN;
-        dy  = (POS_NUM_ROWS + BOARD_TOP_OFFSET) * SQUARE_HEIGHT + TOP_MARGIN;
-        sx  = (c - HANDS_BLACK_PAWN) * SQUARE_WIDTH;
-        sy  = (0) * SQUARE_HEIGHT;
-        g_imgScreen.copyRectangle(
-                dx, dy, SQUARE_WIDTH, SQUARE_HEIGHT, g_imgPiece, sx, sy);
-    }
-
-    //  選択しているマスがあれば強調表示。  //
-    if ( (g_selX >= 0) && (g_selY >= 0) ) {
-        sx  = ((g_selX) * SQUARE_WIDTH) + LEFT_MARGIN;
-        sy  = ((g_selY) * SQUARE_HEIGHT) + TOP_MARGIN;
-
-        g_imgScreen.drawTransparentRectangle(
-                sx,  sy,  SQUARE_WIDTH,  SQUARE_HEIGHT,
-                255, 255, 0, 192);
-    }
-
-    //  移動先として現在マウスが示しているマスを強調表示。  //
-    if ( (g_movX >= 0) && (g_movY >= 0) ) {
-        sx  = ((g_movX) * SQUARE_WIDTH) + LEFT_MARGIN;
-        sy  = ((g_movY) * SQUARE_HEIGHT) + TOP_MARGIN;
-
-        g_imgScreen.drawTransparentRectangle(
-                sx,  sy,  SQUARE_WIDTH,  SQUARE_HEIGHT,
-                0, 0, 255, 192);
-    }
-
-#endif
 
     //  描画した内容を画面に表示する。  //
     g_imgScreen.copyRectangle(
@@ -637,11 +356,6 @@ WinMain(
 
     g_scrBoard.resetGame();
     g_hCapture  = NULL;
-
-    // g_selX  = -1;
-    // g_selY  = -1;
-    // g_movX  = -1;
-    // g_movY  = -1;
 
     //  ウィンドウを表示する。  //
     ::ShowWindow(hWnd, nCmdShow);
