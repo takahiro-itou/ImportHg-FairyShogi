85a3d902d197c92e4dd14164580c9abf0292944c
Takahiro Itou <Takahiro Itou@localhost>
2016-02-17 20:15:15 +0900

    ??????????

--- setuplinks.sh
+++ setuplinks.sh
@@ -9,6 +9,21 @@ LN_S='ln -sf '
 RM_F='rm -f '
 
 ##
+##  シンボリックリンクの確認
+##
+
+${RM_F}  .Config/TestConf.txt
+touch    .Config/TestConf.txt
+${LN_S}  .Config/TestConf.txt  .Config/TestLink
+if [ -L  .Config/TestLink ] ; then
+    echo  'Symbolic Link Enabled.'
+else
+    echo  'Cannot Create Symbolic Link. Use Hard Link...'
+    LN_S=${LN_H}
+fi
+${RM_F}  .Config/TestConf.txt  .Config/TestLink
+
+##
 ##  定数宣言。
 ##
 
@@ -20,7 +35,11 @@ testcommondir='.TestsCommon'
 
 if [ ! -d  Lib/${testcommondir}/Tests ] ; then
     mkdir  -p  Lib/${testcommondir}/Tests
+fi
+
+if [ -d  Lib/${testcommondir}/Tests ] ; then
     (cd  Lib/${testcommondir}/Tests  \
+      &&  ${RM_F}  Makefile.am  \
       &&  ${LN_S}  ../../Tests/.TestsCommon/Makefile.am)
 fi
 
