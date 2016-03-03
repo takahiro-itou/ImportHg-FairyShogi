f90df6e6173240f4a81b61d51e5e23cefb267b29
Takahiro Itou <Takahiro Itou@localhost>
2016-03-03 22:03:27 +0900

    ?????????

---
+++ Lib/Engine/EngineBase.cpp
@@ -0,0 +1,29 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of EngineBase class.
+**
+**      @file       Engine/EngineBase.cpp
+**/
+
+#include    "FairyShogi/Engine/EngineBase.h"
+
+FAIRYSHOGI_NAMESPACE_BEGIN
+namespace  Engine  {
+
+//========================================================================
+//
+//    EngineBase  class.
+//
+
+}   //  End of namespace  Engine
+FAIRYSHOGI_NAMESPACE_END
