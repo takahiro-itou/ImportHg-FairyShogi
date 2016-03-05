b237c0988779dd30f8cd9094f4816c579008a7bc
Takahiro Itou <Takahiro Itou@localhost>
2016-03-05 18:06:08 +0900

    ?????????

--- Include/FairyShogi/Engine/EngineBase.h
+++ Include/FairyShogi/Engine/EngineBase.h
@@ -12,7 +12,7 @@
 /**
 **      An Interface of EngineBase class.
 **
-**      @file       Interface/EngineBase.h
+**      @file       Engine/EngineBase.h
 **/
 
 #if !defined( FAIRYSHOGI_ENGINE_INCLUDED_ENGINE_BASE_H )
@@ -21,6 +21,13 @@
 #include    "FairyShogi/Common/FairyShogiTypes.h"
 
 FAIRYSHOGI_NAMESPACE_BEGIN
+
+//  クラスの前方宣言。  //
+namespace  Common  {
+struct  ActionView;
+struct  ViewBuffer;
+}   //  End of namespace  Common
+
 namespace  Engine  {
 
 //========================================================================
@@ -34,6 +41,93 @@ namespace  Engine  {
 class  EngineBase
 {
 
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    EngineBase();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~EngineBase();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   コンピュータの思考を開始する。
+    **
+    **  @param [in] vbCur     現在の局面。
+    **  @param[out] actRet    思考した結果を受け取る変数。
+    **  @return     エラーコードを返す。
+    **      -   異常終了の場合は、
+    **          エラーの種類を示す非ゼロ値を返す。
+    **      -   正常終了の場合は、ゼロを返す。
+    **/
+    virtual  ErrCode
+    computeBestAction(
+            const   ViewBuffer  &vbCur,
+            ActionView          &actRet);
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+    typedef     EngineBase      This;
+    EngineBase          (const  This  &);
+    This &  operator =  (const  This  &);
+public:
+    //  テストクラス。  //
+    friend  class   EngineBaseTest;
 };
 
 }   //  End of namespace  Engine
