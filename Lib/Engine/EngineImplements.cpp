f56a6b8762646f5f57afe7ac324a39934dc713ef
Takahiro Itou <Takahiro Itou@localhost>
2016-03-08 21:53:45 +0900

    ?????????

---
+++ Lib/Engine/EngineImplements.cpp
@@ -0,0 +1,74 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of EngineImplements class.
+**
+**      @file       Engine/EngineImplements.cpp
+**/
+
+#include    "EngineImplements.h"
+
+FAIRYSHOGI_NAMESPACE_BEGIN
+namespace  Engine  {
+
+//========================================================================
+//
+//    EngineImplements  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Engine
+FAIRYSHOGI_NAMESPACE_END
---
+++ Lib/Engine/EngineImplements.h
@@ -0,0 +1,103 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of EngineImplements class.
+**
+**      @file       Engine/EngineImplements.h
+**/
+
+#if !defined( FAIRYSHOGI_ENGINE_INCLUDED_ENGINE_IMPLEMENTS_H )
+#    define   FAIRYSHOGI_ENGINE_INCLUDED_ENGINE_IMPLEMENTS_H
+
+#include    "FairyShogi/Engine/EngineBase.h"
+
+FAIRYSHOGI_NAMESPACE_BEGIN
+namespace  Engine  {
+
+//========================================================================
+//
+//    EngineImplements  class.
+//
+/**
+**    思考エンジンの実装クラス。
+**/
+
+class  EngineImplements
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+    typedef     EngineImplements    This;
+    EngineImplements    (const  This  &);
+    This &  operator =  (const  This  &);
+public:
+    //  テストクラス。  //
+    friend  class   EngineImplementsTest;
+};
+
+}   //  End of namespace  Engine
+FAIRYSHOGI_NAMESPACE_END
+
+#endif
