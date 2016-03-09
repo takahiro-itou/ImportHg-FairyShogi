4846e23f11d519ff36d3afb0cc3e2a93dff32e85
Takahiro Itou <Takahiro Itou@localhost>
2016-03-09 22:12:31 +0900

    ????????????

--- Lib/Engine/EngineLevel0.cpp
+++ Lib/Engine/EngineLevel0.cpp
@@ -219,75 +219,10 @@ EngineLevel0::computeBestAction(
 //    Accessors.
 //
 
-
 //========================================================================
 //
 //    Protected Member Functions.
 //
-//
-
-//----------------------------------------------------------------
-//    現在の局面の合法手を列挙する。
-//
-
-ErrCode
-EngineLevel0::makeLegalActionList(
-        const  BoardState   &curStat,
-        const  PlayerIndex  piTurn,
-        const  TConstraint  vCons,
-        ActionViewList      &actList)
-{
-    typedef     std::vector<BoardState::ActionData> ActionDataList;
-    typedef     ActionDataList::const_iterator      ActIter;
-
-    //  まず、全ての合法手を列挙する。  //
-    ActionDataList  vActs;
-    ErrCode         retErr  =  ERR_SUCCESS;
-
-
-    retErr  =  curStat.makeLegalActionList(
-                    piTurn,  Common::ALF_LEGAL_ACTION,  vActs);
-
-    actList.clear();
-    actList.reserve( vActs.size() );
-
-    if ( retErr != ERR_SUCCESS ) {
-        return ( retErr );
-    }
-
-    const  ActIter  itrEnd  = vActs.end();
-    ActIter         itrHead = vActs.begin();
-    ActIter         itrTail = itrEnd;
-
-    ActionView      actView;
-    for ( ; itrHead != itrEnd; ++ itrHead ) {
-        BoardState::decodeActionData( (* itrHead),  &actView );
-        if ( (actView.xPlayNewCol) >= vCons )  {
-            break;
-        }
-    }
-
-    for ( ActIter itr = itrHead; itr != itrEnd; ++ itr ) {
-        BoardState::decodeActionData( (* itr),  &actView );
-        if ( (actView.xPlayNewCol) != vCons ) {
-            itrTail = itr;
-            break;
-        }
-    }
-
-    if ( (itrHead == itrTail) ) {
-        //  指定した条件で合法手がない時は、何でもできる。  //
-        itrHead = vActs.begin();
-        itrTail = itrEnd;
-    }
-
-    for ( ActIter itr = itrHead; itr != itrTail; ++ itr ) {
-        BoardState::decodeActionData( (* itr),  &actView );
-        actList.push_back(actView);
-    }
-
-    return ( ERR_SUCCESS );
-}
 
 //========================================================================
 //
