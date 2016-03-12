86516e5d2a50398de9e725332fd6445c3110703e
Takahiro Itou <Takahiro Itou@localhost>
2016-03-12 10:17:35 +0900

    ??????

---
+++ Include/FairyShogi/Common/HelperMacros.h
@@ -0,0 +1,28 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Helper Macros and Functions.
+**
+**      @file       Common/HelerMacros.h
+**/
+
+#if !defined( FAIRYSHOGI_COMMON_INCLUDED_HELPER_MACROS_H )
+#    define   FAIRYSHOGI_COMMON_INCLUDED_HELPER_MACROS_H
+
+#include    "FairyShogiTypes.h"
+
+FAIRYSHOGI_NAMESPACE_BEGIN
+
+
+FAIRYSHOGI_NAMESPACE_END
+
+#endif
