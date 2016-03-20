c22410a48f20e58163c844035948b88ca67ace31
Takahiro Itou <Takahiro Itou@localhost>
2016-03-20 17:20:25 +0900

    ?????????

---
+++ Bin/AutoMatch.cpp
@@ -0,0 +1,27 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      思考エンジン自動対局。
+**
+**      @file       Bin/AutoMatch.cpp
+**/
+
+#include    "FairyShogi/Interface/GameController.h"
+
+#include    <iostream>
+
+using   namespace   FAIRYSHOGI_NAMESPACE;
+
+int  main(int argc, char * argv[])
+{
+    return ( 0 );
+}
