b9a33c5082da1db0bac5b752f3cfab84e2561c4c
Takahiro Itou <Takahiro Itou@localhost>
2016-03-21 14:11:59 +0900

    ????????????????

---
+++ Lib/Engine/EngineLevel0.cpp
@@ -0,0 +1,137 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of EngineLevel0 class.
+**
+**      @file       Engine/EngineLevel0.cpp
+**/
+
+#include    "EngineLevel0.h"
+
+#include    "FairyShogi/Common/ActionView.h"
+#include    "FairyShogi/Game/BoardState.h"
+
+#include    <iostream>
+#include    <time.h>
+
+FAIRYSHOGI_NAMESPACE_BEGIN
+namespace  Engine  {
+
+//========================================================================
+//
+//    EngineLevel0  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+EngineLevel0::EngineLevel0()
+    : Super(),
+      m_rndGen()
+{
+    this->m_rndGen.setSeedValue( ::time(nullptr) );
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+EngineLevel0::~EngineLevel0()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//----------------------------------------------------------------
+//    コンピュータの思考を開始する。
+//
+
+ErrCode
+EngineLevel0::computeBestAction(
+        const  ViewBuffer   &vbCur,
+        const  PlayerIndex  piTurn,
+        const  TConstraint  vCons,
+        ActionView          &actRet)
+{
+    //  現在の局面を、内部形式に変換する。  //
+    BoardState      curStat;
+    ActionViewList  actList;
+
+    curStat.encodeFromViewBuffer(vbCur);
+
+    actList.clear();
+    makeLegalActionList(curStat,  piTurn,  vCons,  actList);
+
+    const  int  numAct  =  actList.size();
+    if ( numAct == 0 ) {
+        return ( ERR_ILLEGAL_ACTION );
+    }
+    if ( numAct == 1 ) {
+        actRet  =  actList[0];
+        return ( ERR_SUCCESS );
+    }
+
+    //  合法手の中からランダムに選択する。  //
+    int  r  =  (this->m_rndGen.getNext()) % numAct;
+    actRet  =  actList[r];
+
+    return ( ERR_SUCCESS );
+}
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Engine
+FAIRYSHOGI_NAMESPACE_END
---
+++ Lib/Engine/EngineLevel0.h
@@ -0,0 +1,148 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of EngineLevel0 class.
+**
+**      @file       Engine/EngineLevel0.h
+**/
+
+#if !defined( FAIRYSHOGI_ENGINE_INCLUDED_ENGINE_LEVEL_0_H )
+#    define   FAIRYSHOGI_ENGINE_INCLUDED_ENGINE_LEVEL_0_H
+
+#include    "EngineImplements.h"
+
+#include    "FairyShogi/Common/MersenneTwister.h"
+
+FAIRYSHOGI_NAMESPACE_BEGIN
+namespace  Engine  {
+
+//========================================================================
+//
+//    EngineLevel0  class.
+//
+/**
+**    思考エンジンクラス。
+**/
+
+class  EngineLevel0 : public  EngineImplements
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    EngineLevel0();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~EngineLevel0();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   コンピュータの思考を開始する。
+    **
+    **  @param [in] vbCur     現在の局面。
+    **  @param [in] piTurn    現在の手番のプレーヤー番号。
+    **  @param [in] vCons     制約条件。
+    **  @param[out] actRet    思考した結果を受け取る変数。
+    **  @return     エラーコードを返す。
+    **      -   異常終了の場合は、
+    **          エラーの種類を示す非ゼロ値を返す。
+    **      -   正常終了の場合は、ゼロを返す。
+    **/
+    virtual  ErrCode
+    computeBestAction(
+            const  ViewBuffer   &vbCur,
+            const  PlayerIndex  piTurn,
+            const  TConstraint  vCons,
+            ActionView          &actRet)  OVERRIDE;
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+private:
+    typedef     Common::MersenneTwister     RandomGenerator;
+
+    RandomGenerator     m_rndGen;
+
+//========================================================================
+//
+//    Other Features.
+//
+private:
+    /**   スーパークラス。  **/
+    typedef     EngineImplements    Super;
+
+private:
+    typedef     EngineLevel0        This;
+    EngineLevel0        (const  This  &);
+    This &  operator =  (const  This  &);
+public:
+    //  テストクラス。  //
+    friend  class   EngineLevel0Test;
+};
+
+}   //  End of namespace  Engine
+FAIRYSHOGI_NAMESPACE_END
+
+#endif
