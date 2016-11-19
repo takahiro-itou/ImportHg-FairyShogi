60656208850110ee80180378a35bc3ede14dd937
Takahiro Itou <Takahiro Itou@localhost>
2016-11-19 17:03:37 +0900

    ???????? (1) ????????????????????????????     ??????????????????

--- .CMake/CheckOverride.cmake
+++ .CMake/CheckOverride.cmake
@@ -1,17 +1,17 @@
 
-include (CheckCXXSourceCompiles)
+Include (CheckCXXSourceCompiles)
 
-check_cxx_source_compiles (
-  "class A {
-    virtual void f() { }
-  };
-  class B : public A {
-    virtual void f()  override { }
-  };
-  int main () {
-    return ( 0 );
-  }"
-  CNF_CXX_OVERRIDE_ENABLED
+Check_CXX_Source_Compiles (
+   "class A { virtual void f() { } };
+    class B : public A { virtual void f() override { } };
+    int main () {
+        return ( 0 );
+    }"
+    CMAKE_CHECK_CXX_OVERRIDE_ENABLED
 )
-
+If ( CMAKE_CHECK_CXX_OVERRIDE_ENABLED )
+    Set (CONFIG_CHECK_CXX_OVERRIDE_ENABLED  1)
+Else  ()
+    Set (CONFIG_CHECK_CXX_OVERRIDE_ENABLED  0)
+EndIf ()
 
