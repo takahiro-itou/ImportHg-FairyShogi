06f39acafe86da01dacb1107abb860b035c93563
Takahiro Itou <Takahiro Itou@localhost>
2016-11-23 18:55:20 +0900

    ?????????

---
+++ Bin/TextUI/FairyShogiCurses.cpp
@@ -0,0 +1,25 @@
+//  -*-  coding: utf-8; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Fairy Shogi Game  ---                      **
+**                                                                      **
+**                                                                      **
+**          Copyright (C), 2015-2016, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      アプリケーション本体。
+**
+**      @file       Bin/FairyShogi.cpp
+**/
+
+#include    "FairyShogi/Helper/TerminalScreen.h"
+
+using   namespace   FAIRYSHOGI_NAMESPACE;
+
+int  main(int argc, char * argv[])
+{
+    return ( 0 );
+}
